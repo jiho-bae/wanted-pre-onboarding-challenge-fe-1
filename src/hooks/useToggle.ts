74eb9414d30397fc76e4d88e7d52a5470ca25afb
jiho-bae <jiho@dgu.ac.kr>
2023-01-18 15:25:13 +0900

    FEat: useToggle hook 추가

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,8 @@
+import { useCallback, useState } from 'react';
+
+export default function useToggle(initialValue = false): [boolean, () => void] {
+  const [state, setState] = useState(initialValue);
+  const toggle = useCallback(() => setState((state) => !state), []);
+
+  return [state, toggle];
+}
